1ba5b2d48088a306a7aa54b82c86ded645decea1
nbrick <nebricks@gmail.com>
2015-03-04 17:27:43 +0000

    Add exercises, Image docs link; move logo to img/

--- src/image-processing/invert.py
+++ src/image-processing/invert.py
@@ -1,6 +1,8 @@
 import itertools
 import math
 
+# View documentation for the Image module at:
+# http://effbot.org/imagingbook/image.htm
 from PIL import Image
 
 
@@ -16,6 +18,18 @@ def invert_left_half(image):
         pixel_rgba_values[i, j] = (255-r, 255-g, 255-b, a)
 
 
-with Image.open("cucats-logo.png") as image:
+with Image.open("img/cucats-logo.png") as image:
     invert_left_half(image)
-    image.save("cucats-logo-half-inverted.png")
+    image.save("img/cucats-logo-half-inverted.png")
+
+
+# Exercises
+# ---------
+#
+# 1) Crop the CUCaTS logo, to get rid of the text.
+#
+# 2) Try using a different input image. Something will go wrong if you
+#    don't use a png with transparency. What goes wrong and how can you
+#    fix it?
+#
+# 3) Try blurring an image.
